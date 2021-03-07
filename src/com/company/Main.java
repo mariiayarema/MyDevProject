29baf5b6fce074816360470468cb6034252ed1f1
mariiayarema <MYarema1991@gmail.com>
2021-03-07 00:42:43 -0500

    My third Jenkins commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,6 +6,7 @@ public class Main {
 // write your code here
         for (int i = 0; i <5 ; i++) {
             System.out.println("Hello batch 8. How are you?");
+            System.out.println("Very good");
             Thread.sleep(2000);
         }
     }
