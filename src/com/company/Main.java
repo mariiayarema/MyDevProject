21e9fed526f4a3bc7d1355dac5b147b4d0980adf
mariiayarema <MYarema1991@gmail.com>
2021-03-07 00:54:36 -0500

    My forth Jenkins commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) throws InterruptedException {
 // write your code here
         for (int i = 0; i <5 ; i++) {
-            System.out.println("Hello batch 8. How are you?");
+            ystem.out.println("Hello batch 8. How are you?");
             System.out.println("Very good");
             Thread.sleep(2000);
         }
